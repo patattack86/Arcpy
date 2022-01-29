9bec17b1ac55491315bc8a8b13cce787f42e05fe
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2022-01-29 13:22:23 -0500

    Create Add_tiles_to_RasterMosaice.py

---
+++ Add_tiles_to_RasterMosaice.py
@@ -0,0 +1,28 @@
+import boto3
+import arcpy
+import os
+from botocore.handlers import disable_signing
+
+s3 = boto3.resource('s3')
+s3.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
+
+bucket = s3.Bucket('njogis-imagery')
+
+my_bucket = s3.Bucket(bucket)
+
+mosaic_dataset = r"B:\Woolpert\ArcProPython\Tutorial_geodatabse.gdb\youtube_dataset"
+
+prefix = "2015/cog"
+
+counter = 0
+
+for file in my_bucket.objects.filter(Prefix=prefix):
+    counter += 1
+    vsis3_image_path = os.path.join("/vsis3/", bucket, file.key). replace('\\','/')
+    print("Adding {}". format(vsis3_image_path))
+    print("counter = {}". format(counter))
+    arcpy.AddRastersToMosaicDataset_management(mosaic_dataset, "Raster Dataset", vsis3_image_path)
+    if counter > 5:
+        break
+
+print("Program Finished")
